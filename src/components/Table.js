7eaaadf88c33fb4f871aeda395f1ba65d61028cd
Mayara Marques <mayaramarquescosta@outlook.com>
2022-08-31 15:58:32 -0300

    Requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,24 +1,64 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div>
         Table
         <table>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
+          <thead>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </thead>
+          <tbody>
+            {expenses.map((el, index) => (
+
+              <tr key={ index }>
+                <td>{el.description}</td>
+                <td>{el.tag}</td>
+                <td>{el.method}</td>
+                <td>
+                  {
+                    el.value === '' ? '0.00' : parseFloat(el.value).toFixed(2)
+                  }
+                </td>
+                <td>{el.exchangeRates[el.currency].name}</td>
+                <td>
+                  {
+                    parseFloat(el.exchangeRates[el.currency].ask).toFixed(2)
+                  }
+
+                </td>
+                <td>
+                  {parseFloat(el.exchangeRates[el.currency]
+                    .ask * el.value).toFixed(2)}
+                </td>
+                <td>Real brasileiro</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape).isRequired,
+};
+
+export default connect(mapStateToProps, null)(Table);
---
+++ src/tests/helpers/index.test.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import userEvent from '@testing-library/user-event';
+// import { getByRole, screen } from '@testing-library/react';
+// import Header from '../../components/Header';
+// import App from '../../App';
+// import renderWithRouterAndRedux from './renderWith';
+// import Wallet from '../../pages/Wallet';
+// import Table from '../../components/Table';
+// import WalletForm from '../../components/WalletForm';
+
+// describe('Verifica se os testes estão cobrindo os componentes', () => {
+//   it('Verifica se o component App está sendo renderizado na tela', () => {
+//     renderWithRouterAndRedux(<App />);
+//     expect(App).toBeInTheDocument();
+//   });
+//   it('Verifica se tem um input email na tela inicial', () => {
+//     const email = screen.getByRole()
+//   })
+// });
